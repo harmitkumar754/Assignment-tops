179d2885b3d2cb281f1c02e1c6d0c871d4fd3c4d
Harmitkumar Donda <harmitdonda686@gmail.com>
2025-01-08 23:27:18 +0530

    Add files via upload

---
+++ Module-14) React - Components, State, Props,Handling Events/src/src/q1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Q1() {
+  return (
+    <div>
+        <h3><u>Question:1</u> Set up a new React.js project using create-react-app.</h3>
+    </div>
+  )
+}
